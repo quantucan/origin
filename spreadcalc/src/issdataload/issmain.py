1aca976b84647abe8fbe047d216281a0dc561143
Alexander Yusupoff <yusupov.ag@gmail.com>
2020-07-30 22:16:53 +0500

    Exclude csv and xml parse projects from tracked files

--- spreadcalc/src/issdataload/issmain.py
+++ spreadcalc/src/issdataload/issmain.py
@@ -12,10 +12,10 @@ def main():
     #iss.check_iss_moex(url='http://iss.moex.com/iss/engines/stock/markets/bonds/securities.json')
     #iss.load_emitents_info(cnx, url='http://iss.moex.com/iss/securities.json?securities.columns=shortname,regnumber,isin,emitent_id,emitent_inn,emitent_title&engine=stock&market=bonds&is_trading=1')
         
-    iss.update_bonds_info()
+    #iss.update_bonds_info()
     #iss._duplicate_boards(datetime.date(2020, 6, 22))
     
-    '''trdate = datetime.date(2020, 7, 27)  
+    trdate = datetime.date(2020, 7, 30)  
     for i in range(1):        
         res = iss.load_end_of_day(trdate)
         if res == False:
@@ -23,7 +23,7 @@ def main():
         else:
             print(trdate, 'OK')
             
-        trdate += datetime.timedelta(days = 1)'''
+        trdate += datetime.timedelta(days = 1)
     
 if __name__ == '__main__':
     try:
