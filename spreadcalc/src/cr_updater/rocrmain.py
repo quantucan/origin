75114a09e32d04e2f295bcb83fa7b694eac7deb0
Alexander Yusupoff <yusupov.ag@gmail.com>
2020-08-13 21:25:35 +0500

    Read LEI codes from database and parse S&P records of credit rating actions

---
+++ spreadcalc/src/cr_updater/rocrmain.py
@@ -0,0 +1,14 @@
+'''
+Created on 5 авг. 2020 г.
+
+@author: Александр
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as err:
+        print('Exception:', err)
