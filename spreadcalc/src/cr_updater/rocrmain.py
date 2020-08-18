47a0a980832ee64cdc44744da7ab865f401d8f9f
Alexander Yusupoff <yusupov.ag@gmail.com>
2020-08-18 22:01:15 +0500

    Record distinct emitents S&P credit ratings

--- spreadcalc/src/cr_updater/rocrmain.py
+++ spreadcalc/src/cr_updater/rocrmain.py
@@ -9,56 +9,45 @@ from datetime import date
 from lxml import etree
 
 def main():
-    sp_rating_path = '.\\SP'
+    sp_rating_path = r'.\SP'
     ns = {'r' : 'http://xbrl.sec.gov/ratings/2015-03-31'}
-    emt = dict()
     
     cnx = mysql.connector.connect(user = 'root', password = 'admiN123', database = 'bondinfo', host = '127.0.0.1', port = 3306)
     cur = cnx.cursor()
     
-    #emitents from database
-    cur.execute('SELECT lei, id FROM emitents WHERE lei IS NOT NULL ORDER BY id ASC')    
-    for row in cur:
-        emt[row[0]] = row[1]
+    fname = 'SP-NAME-YY287944738-OJSC-First-Collection-Bureau-2020-08-01-OBLIGOR.xml'
+    emitent_id = 204
     
-    #files
-    rfiles = [os.path.join(sp_rating_path, r) for r in os.listdir(sp_rating_path) if os.path.isfile(os.path.join(sp_rating_path, r)) and r.endswith('OBLIGOR.xml')]
+    rfile = os.path.join(sp_rating_path, fname)    
     
-    for rf in rfiles:
-        rocra = etree.parse(rf)        
+    rocra = etree.parse(rfile)        
             
-        lei_rec = rocra.xpath(r'r:ROCRA/r:OD/r:LEI', namespaces=ns)
-        if len(lei_rec) == 0:
-            continue
-        else:
-            lei_code = lei_rec[0].text
-            emitent_id = emt.pop(lei_code, None)
-            if emitent_id is None:
-                continue
-            else:                
-                lt_ord_recs = rocra.xpath(r'r:ROCRA/r:OD/r:ORD[r:RT="Issuer Credit Rating" and r:RST="Foreign Currency LT"]', namespaces=ns)
-                for ord_rec in lt_ord_recs:                    
-                    rd = {'emitent_id' : emitent_id, 'rad' : None, 'rating' : None, 'rol' : None, 'wst' : None}
+    #lei_rec = rocra.xpath(r'r:ROCRA/r:OD/r:LEI', namespaces=ns)        
+    #lei_code = lei_rec[0].text
+        
+    lt_ord_recs = rocra.xpath(r'r:ROCRA/r:OD/r:ORD[r:RT="Issuer Credit Rating" and r:RST="Foreign Currency LT"]', namespaces=ns)
+    for ord_rec in lt_ord_recs:                    
+        rd = {'emitent_id' : emitent_id, 'rad' : None, 'rating' : None, 'rol' : None, 'wst' : None}
                     
-                    for i in ord_rec:
-                        tag = etree.QName(i).localname
+        for i in ord_rec:
+            tag = etree.QName(i).localname
                         
-                        if tag == 'R':
-                            rd['rating'] = i.text
-                        elif tag == 'RAD':
-                            rd['rad'] = date.fromisoformat(i.text)
-                        elif tag == 'WST':
-                            if i.text is None:
-                                continue
-                            else:
-                                rd['wst'] = i.text.lower()
-                        elif tag == 'ROL':
-                            if i.text is None:
-                                continue
-                            else:
-                                rd['rol'] = i.text.lower()                    
+            if tag == 'R':
+                rd['rating'] = i.text
+            elif tag == 'RAD':
+                rd['rad'] = date.fromisoformat(i.text)
+            elif tag == 'WST':
+                if i.text is None:
+                    continue
+                else:
+                    rd['wst'] = i.text.lower()
+            elif tag == 'ROL':
+                if i.text is None:
+                    continue
+                else:
+                    rd['rol'] = i.text.lower()                    
                                         
-                    cur.execute('INSERT INTO sp (emitent_id, rad, rating, rol, wst) VALUES (%(emitent_id)s, %(rad)s, %(rating)s, %(rol)s, %(wst)s)', rd)
+        cur.execute('INSERT INTO sp (emitent_id, rad, rating, rol, wst) VALUES (%(emitent_id)s, %(rad)s, %(rating)s, %(rol)s, %(wst)s)', rd)
                         
     cnx.commit()
     cnx.close()
