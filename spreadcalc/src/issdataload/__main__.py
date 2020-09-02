2c30b8dda8bbe1e5d357a9777a0780af8b19fdf3
Alexander Yusupoff <yusupov.ag@gmail.com>
2020-09-02 19:52:59 +0500

    Change script flow execution in __main__.py

--- spreadcalc/src/issdataload/__main__.py
+++ spreadcalc/src/issdataload/__main__.py
@@ -7,35 +7,27 @@ import sys
 import datetime
 from issdataload.issclient import MoexISSClient
 
-upd_flag = 0
+def main(cmd_args):
+    iss = MoexISSClient()
+    
+    if not iss.connect():
+        print('Can\'t connect to bondinfo database')        
+        return
+    
+    if len(cmd_args) == 2 and cmd_args[1] == '--updbonds':
+        try:
+            iss.update_bonds_info()
+            print('Bondinfo database has been updated')
+        except Exception as err:
+            print('Exception:', err.msg)
+    elif len(cmd_args) == 3 and cmd_args[1] == '--updquotes':        
+        try:
+            trdate = datetime.date.fromisoformat(cmd_args[2])
+            res = iss.load_end_of_day(trdate)
+            if res:
+                print(trdate, 'OK')
+        except Exception as err:
+                print('Exception:', err)
 
-if len(sys.argv) == 2 and sys.argv[1] == '--updbonds':
-    upd_flag = 1
-elif len(sys.argv) == 3 and sys.argv[1] == '--updquotes':
-    upd_flag = 2
-    try:
-        trdate = datetime.date.fromisoformat(sys.argv[2])
-    except Exception as err:        
-        print('Exception:', err)
-        sys.exit()      
-
-iss = MoexISSClient()
-
-if not iss.connect():
-    print('Can\'t connect to bondinfo database')
-    sys.exit()
-
-if upd_flag == 1:
-    try:
-        iss.update_bonds_info()
-        print('Bondinfo database has been updated')
-    except Exception as err:
-        print('Exception:', err)    
-
-elif upd_flag == 2:    
-    try:
-        res = iss.load_end_of_day(trdate)
-        if res:
-            print(trdate, 'OK')
-    except Exception as err:
-        print('Exception:', err)
+if __name__ == '__main__':
+    main(sys.argv)
